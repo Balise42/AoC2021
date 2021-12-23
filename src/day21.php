51537ed05e4776bf425b44cf02901e369bb451ff
Isabelle Hurbain-Palatin <isabelle@palatin.fr>
2021-12-23 18:23:40 +0100

    day 21 ** and day 22

--- src/day21.php
+++ src/day21.php
@@ -37,9 +37,13 @@ function day21a(int $p1, int $p2) {
     }
 }
 
-function numWins(int $p1, int $p2, int $s1, int $s2, int $limit, array &$memoWins ) {
-    if ( isset ( $memoWins[$p1][$p2][$s1][$s2]  ) ) {
-        return $memoWins[$p1][$p2][$s1][$s2];
+global $memoWins;
+
+function numWins(int $p, int $p1, int $p2, int $s1, int $s2, int $limit ) {
+    global $memoWins;
+
+    if ( isset ( $memoWins[$p1][$p2][$s1][$s2][$p]  ) ) {
+        return $memoWins[$p1][$p2][$s1][$s2][$p];
     }
     if ( !isset($memoWins[$p1])) {
         $memoWins[$p1] = [];
@@ -47,20 +51,26 @@ function numWins(int $p1, int $p2, int $s1, int $s2, int $limit, array &$memoWin
     if ( !isset($memoWins[$p1][$p2])) {
         $memoWins[$p1][$p2] = [];
     }
-    if ( !isset($memoWins[$p1][$s1])) {
+    if ( !isset($memoWins[$p1][$p2][$s1])) {
         $memoWins[$p1][$p2][$s1] = [];
+    }
+    if ( !isset($memoWins[$p1][$p2][$s1][$s2])) {
+        $memoWins[$p1][$p2][$s1][$s2] = [];
     }
 
     if ( $s1 >= $limit ) {
-        $memoWins[$p1][$p2][$s1][$s2] = 1;
-        return 1;
+        $memoWins[$p1][$p2][$s1][$s2][1] = 1;
+        $memoWins[$p1][$p2][$s1][$s2][2] = 0;
+        return $memoWins[$p1][$p2][$s1][$s2][$p];
     }
     if ($s2 >= $limit ) {
-        $memoWins[$p1][$p2][$s1][$s2] = 0;
-        return 0;
+        $memoWins[$p1][$p2][$s1][$s2][1] = 0;
+        $memoWins[$p1][$p2][$s1][$s2][2] = 1;
+        return $memoWins[$p1][$p2][$s1][$s2][$p];
     }
 
-    $wins = 0;
+    $winsP1 = 0;
+    $winsP2 = 0;
     for ( $i = 1; $i <= 3; $i++ ) {
         for ( $j = 1; $j <= 3; $j++ ) {
             for ( $k = 1; $k <= 3; $k++ ) {
@@ -69,10 +79,11 @@ function numWins(int $p1, int $p2, int $s1, int $s2, int $limit, array &$memoWin
                     $pos1 = $pos1 - 10;
                 }
                 $newS1 = $s1 + $pos1;
-                if ( $newS1 >= $limit ) {
-                    $wins++;
+                if ( $newS1 >= $limit) {
+                    $winsP1++;
                     continue;
                 }
+
                 for ( $l = 1; $l <= 3; $l++ ) {
                     for ( $m = 1; $m <= 3; $m++ ) {
                         for ( $n = 1; $n <= 3; $n++ ) {
@@ -81,26 +92,25 @@ function numWins(int $p1, int $p2, int $s1, int $s2, int $limit, array &$memoWin
                                 $pos2 = $pos2 - 10;
                             }
                             $newS2 = $s2 + $pos2;
-                            if ( $newS2 > $limit ) {
-                                continue;
-                            }
-                            $wins = $wins + numWins( $pos1, $pos2, $newS1, $newS2, 21, $memoWins);
+
+                            $winsP1 += numWins( 1, $pos1, $pos2, $newS1, $newS2, $limit);
+                            $winsP2 += numWins( 2, $pos1, $pos2, $newS1, $newS2, $limit);
                         }
                     }
                 }
             }
         }
     }
-    $memoWins[$p1][$p2][$s1][$s2] = $wins;
-    return $wins;
+    $memoWins[$p1][$p2][$s1][$s2][1] = $winsP1;
+    $memoWins[$p1][$p2][$s1][$s2][2] = $winsP2;
+
+    return $memoWins[$p1][$p2][$s1][$s2][$p];
 }
 
 function day21b( $p1, $p2 ) {
-    $memoWins = [];
-    $a = numWins( $p1, $p2, 0, 0, 21, $memoWins );
-    //$b = numWins( $p2, $p1, 21, 21,  $memoWins );
-    //return ($a > $b) ? $a : $b;
-    return $a;
+    $a = numWins( 1, $p1, $p2, 0, 0, 21 );
+    $b = numWins( 2, $p1, $p2, 0, 0, 21 );
+    return ($a > $b) ? $a : $b;
 }
 
 function day21(int $p1, int $p2) {
@@ -108,4 +118,4 @@ function day21(int $p1, int $p2) {
     print day21b( $p1, $p2 ) . PHP_EOL;
 }
 
-day21(4, 8 );
+day21(2, 1 );
--- src/day22.php
+++ src/day22.php
@@ -1,6 +1,7 @@
 <?php
 
-class Cube {
+class Cube
+{
     public $xmin;
     public $xmax;
     public $ymin;
@@ -8,19 +9,63 @@ class Cube {
     public $zmin;
     public $zmax;
 
-    public function contains( int $x, int $y, int $z ) : bool {
+    public function contains(int $x, int $y, int $z): bool
+    {
         return $x >= $this->xmin && $x <= $this->xmax && $y >= $this->ymin && $y <= $this->ymax && $z >= $this->zmin && $z <= $this->zmax;
     }
 
-    public function intersects(Cube $c) {
-        return $this->contains( $c->xmin, $c->ymin, $c->zmin)
-            || $this->contains( $c->xmax, $c->ymin, $c->zmin)
-            || $this->contains( $c->xmin, $c->ymax, $c->zmin)
-            || $this->contains( $c->xmin, $c->ymin, $c->zmax)
-            || $this->contains( $c->xmax, $c->ymax, $c->zmin)
-            || $this->contains( $c->xmax, $c->ymin, $c->zmax)
-            || $this->contains( $c->xmin, $c->ymax, $c->zmax)
-            || $this->contains( $c->xmax, $c->ymax, $c->zmax);
+    public function containsCube(Cube $c) :bool {
+        return $this->contains($c->xmin, $c->ymin, $c->zmin)
+            && $this->contains($c->xmax, $c->ymax, $c->zmax);
+    }
+
+    public function intersects(Cube $c) : bool
+    {
+        return !($c->xmax < $this->xmin
+            || $c->xmin > $this->xmax
+            || $c->ymax < $this->ymin
+            || $c->ymin > $this->ymax
+            || $c->zmax < $this->zmin
+            || $c->zmin > $this->zmax
+        );
+    }
+
+    public function isValid() :bool {
+        return $this->xmin <= $this->xmax && $this->ymin <= $this->ymax && $this->zmin <= $this->zmax;
+    }
+
+    public function getSplitCubes(Cube $c): array
+    {
+        $xs = [$this->xmin, $this->xmax, $c->xmin, $c->xmax];
+        $ys = [$this->ymin, $this->ymax, $c->ymin, $c->ymax];
+        $zs = [$this->zmin, $this->zmax, $c->zmin, $c->zmax];
+
+        sort($xs);
+        sort($ys);
+        sort($zs);
+
+        $cubes = [];
+        for ($i = 0; $i < 3; $i++) {
+            for ($j = 0; $j < 3; $j++) {
+                for ($k = 0; $k < 3; $k++) {
+                    $cube = new Cube();
+                    $cube->xmin = $xs[$i] + ( $i === 2 ? 1 : 0);
+                    $cube->xmax = $xs[$i + 1] - ( $i === 0 ? 1 : 0 );
+                    $cube->ymin = $ys[$j] + ( $j=== 2 ? 1 : 0);
+                    $cube->ymax = $ys[$j + 1] - ( $j === 0 ? 1 : 0 );
+                    $cube->zmin = $zs[$k] + ( $k === 2 ? 1 : 0);
+                    $cube->zmax = $zs[$k + 1] - ( $k === 0 ? 1 : 0 );
+                    if ($cube->isValid()) {
+                        $cubes[] = $cube;
+                    }
+                }
+            }
+        }
+        return $cubes;
+    }
+
+    public function volume() : int {
+        return ( $this->xmax - $this->xmin + 1 ) * ( $this->ymax - $this->ymin + 1) * ($this->zmax - $this->zmin + 1);
     }
 }
 
@@ -64,8 +109,51 @@ function day22a(array $cubes, array $instr) {
     return $res;
 }
 
-function day22b(array $cubes) {
-    return 0;
+function day22b(array $toInsert, array $instr) : int {
+    $onCubes = [];
+    while ( $toInsert != null ) {
+        /** @var Cube $newCube */
+        $newCube = array_shift( $toInsert );
+        $ins = array_shift( $instr );
+        /** @var Cube $cube */
+        $shouldInsertNewCube = ($ins === 1);
+        foreach ( $onCubes as $key => $cube ) {
+            if ( $newCube->intersects( $cube ) ) {
+                unset ( $onCubes[$key] );
+                $smallCubes = $newCube->getSplitCubes($cube);
+                if ( $ins === 1 ) {
+                    // we're keeping all the cubes that in the union of both cubes
+                    foreach ($smallCubes as $smallCube) {
+                        if ( $cube->containsCube($smallCube)) {
+                            $onCubes[] = $smallCube;
+                        } else if ($newCube->containsCube($smallCube)) {
+                            array_unshift( $toInsert, $smallCube);
+                            array_unshift( $instr, 1);
+                        }
+                    }
+                    $shouldInsertNewCube = false;
+                    break;
+                } else {
+                    foreach ($smallCubes as $smallCube) {
+                        // and here we keep all small cubes already in the "on" but NOT in the new cube
+                        if ($cube-> containsCube($smallCube) && !$newCube->containsCube($smallCube)) {
+                            $onCubes[] = $smallCube;
+                        }
+                    }
+                }
+            }
+        }
+        if ($shouldInsertNewCube) {
+            $onCubes[] = $newCube;
+        }
+    }
+
+    $res = 0;
+
+    foreach ( $onCubes as $cube ) {
+        $res += $cube->volume();
+    }
+    return $res;
 }
 
 function day22(string $filename): void
@@ -93,8 +181,8 @@ function day22(string $filename): void
 
     $day22a = day22a($cubes, $instr);
     print ($day22a . PHP_EOL);
-    $day22b = day22b($cubes);
+    $day22b = day22b($cubes, $instr);
     print ($day22b . PHP_EOL);
 }
 
-day22('../data/day22:q.txt');
+day22('../data/day22.txt');
